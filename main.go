ee3ebdaa0e280943cb3d3ef7e897642c1b556774
navneetshukl <navneetshukla824@gmail.com>
2025-05-25 16:05:10 +0530

    feat:sending url parameter back to client

--- main.go
+++ main.go
@@ -12,37 +12,54 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	fmt.Println("Listening on PORT ", port)
+	fmt.Println("Listening on PORT", port)
 
-	list, err := net.Listen("tcp", fmt.Sprintf("%s:%s", "0.0.0.0", port))
+	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
 	if err != nil {
-		fmt.Println("error in binding the port ", err)
+		fmt.Println("error in binding the port:", err)
 		os.Exit(1)
 	}
 
-	conn, err := list.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
-		fmt.Println("error in accepting the connection ", err)
+		fmt.Println("error in accepting the connection:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("error in reading data from connection:", err)
 		os.Exit(1)
 	}
 
-	buff := make([]byte, 1024)
+	data := string(buffer[:n])
+	fmt.Println("Received request:\n", data)
+	fmt.Println("******************************************************************")
+	reqArray := strings.Split(data, "\r\n")
 
-	_, err = conn.Read(buff)
-	if err != nil {
-		fmt.Println("error in reading data from connection ", err)
-		os.Exit(1)
+	for _, line := range reqArray {
+		fmt.Println(line)
 	}
-
-	fmt.Println("data is ", string(buff))
-
 	fmt.Println("******************************************************************")
 
-	reqArray := strings.Split(string(buff), "\r\n")
-
-	for _, v := range reqArray {
-		fmt.Println(v)
+	lines := strings.Split(data, "\r\n")
+	if len(lines) == 0 || !strings.HasPrefix(lines[0], "GET") {
+		fmt.Println("Invalid request")
+		return
 	}
-	fmt.Println("******************************************************************")
 
+	path := strings.Split(lines[0], " ")[1]
+	body := "Requested path: " + path + "\n"
+
+	response := fmt.Sprintf(
+		"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+		len(body), body,
+	)
+
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("error in writing response:", err)
+	}
 }
